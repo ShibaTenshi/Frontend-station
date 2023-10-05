6dc2012d37c17fe6d3094ab9a13bf62db579b228
Peerasit <peerasitearth@outlook.com>
2023-10-05 23:31:34 +0700

    Update UI login

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,22 @@
 <template>
-    <h1>login</h1>
 
     <div class="box">
         <div class="logo">
-
+            <h4>Log-In</h4>
         </div>
         <div class="field">
             <ul>
-                <li>Username <input type="text" name="usernameField"></li>
-                <li>Password<input type="text" name="passwdField"></li>
+                <div class="textField">
+                    <p>Username</p>
+                    <li><input type="text" name="usernameField"></li>
+                </div>
+                <div class="textField">
+                    <p>Password</p>
+                    <li><input type="text" name="passwdField"></li>
+                </div>
             </ul>
-            <button v-on:click="getUsernameField(),getPasswordField()">Accept</button>
-            <p>If you don’t have your account, Please click <NuxtLink to="/">Sign-Up</NuxtLink></p>
+            <button v-on:click="getUsernameField(),getPasswordField()" class="AcceptBt">Accept</button>
+            <p class="signupLink">If you don’t have your account, Please click <NuxtLink to="/">Sign-Up</NuxtLink></p>
         </div>
     </div>
 
@@ -48,19 +53,79 @@
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        background-color: whitesmoke;
+        background-color: white;
     }
-    input{
-        background-color: gainsboro;
+    li{
+        list-style: none;
     }
-    button {
-        background-color: rgb(148, 166, 132);
-        transition: 0.35s;
-        padding: 10px 10px;
+    div .box{
+        margin: auto;
+        margin-top: 7rem;
+        border: 2px solid rgb(177, 206, 1);
+        justify-content: center;
+        border-radius: 20px;
+        width: 40%;
+        box-shadow: 10px 10px rgb(226, 226, 226);
     }
 
-    button:hover {
+
+    .logo{
+        text-align: center;
+        margin: 4rem;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    .field{
+        text-align: center;
+    }
+    .field ul li {
+        margin-left: 10px;
+    }
+    .textField{
+        display: flex;
+        justify-content: center;
+        margin-top: 2.5rem;
+        margin-bottom: 2rem;
+    }
+    .textField p{
+        font-size: 18px;
+        margin-top: 3px;
+    }
+    .field ul li input{
+        padding: 15px;
+        font-size: 18px;
+        background-color: white;
+        width: 16rem;
+        height: 1.8rem;
+        border-radius: 3rem;
+        border: 1px solid black;
+        outline: none;
+        transition: 0.35s;
+    }
+    .field ul li input:hover{
+        transition: 0.35s;
+        border-color: rgb(151, 151, 151);
+    }
+    .field .AcceptBt{
+        font-weight: bold;
+        font-size: 15px;
+        background-color: rgb(148, 166, 132);
+        transition: 0.35s;
+        padding: 10px 30px;
+        border-radius: 10px;
+        margin-left: 14rem;
+        margin-top: 5px;
+        margin-bottom: 5rem;
+    }
+    .AcceptBt:hover {
         background-color: rgb(121, 142, 102);
         transition: 0.35s;
+        padding: 10px 33px;
+    }
+
+    .signupLink{
+        text-align: center;
+        font-size: 14px;
+        margin: 20px;
     }
 </style>
