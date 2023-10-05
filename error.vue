3bc9f87e2abe62095c29ac8eb297ca076ec7aa16
Peerasit <peerasitearth@outlook.com>
2023-10-05 16:51:18 +0700

    init file

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <h1>404 ERROR</h1>
+        <div class="button">
+            <NuxtLink to="/">GO TO HOMEPAGE</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<style>
+    *{
+        margin: 0;
+        border: 0;
+        box-sizing: border-box;
+        background-color: whitesmoke;
+    }
+
+</style>
