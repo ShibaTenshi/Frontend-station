6645a2bccc8211f53022413d3f4afd51766f1707
Peerasit <peerasitearth@outlook.com>
2023-10-07 20:23:41 +0700

    Update Signup and change dir css to assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>Signup</h1>
+    <div class="signUpBox">
+        <div class="logo">
+            <img src="@/assets/login.png" alt="">
+            <div class="signup">
+                <p>Sign-Up</p>
+                <p>as Restaurant</p>
+            </div>
+        </div>
+        <div class="form">
+            <ul>
+                <li>
+                    <p>Firstname Lastname</p>
+                    <input type="text">
+                </li>
+                <li>
+                    <p>Email</p>
+                    <input type="text">
+                </li>
+                <li>
+                    <p>username</p>
+                    <input type="text">
+                </li>
+                <li>
+                    <p>Password</p>
+                    <input type="text">
+                </li>
+                <li>
+                    <p>Confirm Password</p>
+                    <input type="text">
+                </li>
+            </ul>
+
+        </div>
+    </div>
 </template>
+
+<script>
+
+</script>
+
+<style scoped src="@/assets/styles/signup.css">
+</style>
