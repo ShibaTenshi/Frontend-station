aed4d12fcc715969e6d59907fce4ba964cec1762
Peerasit <peerasitearth@outlook.com>
2023-10-08 15:01:16 +0700

    Update Signup page

---
+++ composables/useSignUpForm.ts
@@ -0,0 +1,3 @@
+export const useSignUpForm = (name:string, email:string, username:string) => {
+    console.log(name, email, username)
+}
--- pages/index.vue
+++ pages/index.vue
@@ -11,33 +11,95 @@
             <ul>
                 <li>
                     <p>Firstname Lastname</p>
-                    <input type="text">
+                    <input type="text" name="nameField" id="nameField">
                 </li>
                 <li>
                     <p>Email</p>
-                    <input type="text">
+                    <input type="text" name="emailField" id="emailField">
                 </li>
                 <li>
                     <p>username</p>
-                    <input type="text">
-                </li>
-                <li>
-                    <p>Password</p>
-                    <input type="text">
-                </li>
-                <li>
-                    <p>Confirm Password</p>
-                    <input type="text">
+                    <input type="text" name="usernameField" id="usernameField">
                 </li>
             </ul>
-
+            <div class="enter">
+                <button v-on:click="signupForm()"><p>Next</p></button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    export default{
+        data() {
+            return{
+                name: null,
+                email: null,
+                username: null
+            }
+        },
+        methods:{
+            getFirstNameField(){
+                const name = document.querySelector("input[name=nameField]").value
+                return name
+            },
+            getEmailField(){
+                const email = document.querySelector("input[name=emailField]").value
+                return email
+            },
+            getUsernameField(){
+                const username = document.querySelector("input[name=usernameField]").value
+                return username
+            },
 
+
+            queryForm(){
+                this.name = this.getFirstNameField()
+                this.email = this.getEmailField()
+                this.username = this.getUsernameField()
+            },
+            clearForm(){
+                const name = document.getElementById("nameField");
+                name.value = ""
+                this.name = null
+
+                const email = document.getElementById("emailField");
+                email.value = ""
+                this.email = null
+
+                const username = document.getElementById("usernameField");
+                username.value = ""
+                this.username = null
+            },
+
+
+            validate(){
+                //test
+            },
+
+            async signupForm(){
+                try {
+                    this.queryForm()
+                    this.validate()
+                    const signupForm = () => {useSignUpForm(this.name, this.email, this.username)} //Api
+                    try {
+                        signupForm()
+                    } catch (e) {
+                        console.log(e)
+                    }
+
+                } catch (e) {
+                    console.log(e)
+                }
+                finally{
+                    this.clearForm()
+                }
+            }
+        }
+    }
 </script>
+
+
 
 <style scoped src="@/assets/styles/signup.css">
 </style>
