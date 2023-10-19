0397b140a53ccf72a2dc09b3b8119f304e4ade18
Peerasit <peerasitearth@outlook.com>
2023-10-19 16:11:48 +0700

    Update Signup Page and Request Api with OTP

--- composables/useSignUpForm.ts
+++ composables/useSignUpForm.ts
@@ -1,24 +1,30 @@
-export const useSignUpForm = async (nameForm:string, emailF:string, usernameF:string, restaurantNameF:string, typeF:string, descriptionF:string, locationF:string, imageF:string) => {
+export const useSignUpForm = async (nameForm:string, emailF:string, usernameF:string, restaurantNameF:string, typeF:string, descriptionF:string, locationF:string, imageLogoF:string, imageMenuF:string, mapLinkF:string ,openDateF:string, closeTimeF:string, openTimeF:string) => {
     //console.log(name, email, username, restaurantName, type ,description, location ,image)
 
     const axios = useNuxtApp().$axios;
-    const api = 'http://10.147.17.139:5041/register/restaurant';
+    const api = 'http://localhost:5041/register/restaurant';
 
     const data = await axios.post(api, {
-        fullName: nameForm,
+        ownerName: nameForm,
         email: emailF,
         username: usernameF,
         restaurantName: restaurantNameF,
         category: typeF,
         description: descriptionF,
         location: locationF,
-        image: imageF
+        imagelink: imageLogoF,
+        menulink: imageMenuF,
+        openTime: openTimeF,
+        closeTime: closeTimeF,
+        mapLink: mapLinkF,
+        openDate: openDateF,
     },{
         headers: {
             'Content-type': 'application/json; charset=UTF-8'
         }
     }
     ).then((response) => {
+        //console.log(response.data)
         return response.data;
     }).catch((error) => {
         return error;
--- pages/3.vue
+++ pages/3.vue
@@ -1,9 +1,40 @@
 <template>
   <Header :logo="true" :login="true"/>
     <div>
-      <h1>page3</h1>
-      <p>{{ text }}</p>
-      <button v-on:click="test3">click</button>
+      <div class="date">
+        <ul>
+          <li>
+            <p>Sunday</p>
+            <input type="checkbox" id="1" value="0">
+          </li>
+          <li>
+            <p>Monday</p>
+            <input type="checkbox" id="2" value="0">
+          </li>
+          <li>
+            <p>Tuesday</p>
+            <input type="checkbox" id="3" value="0">
+          </li>
+          <li>
+            <p>Wednesday</p>
+            <input type="checkbox" id="4" value="0">
+          </li>
+          <li>
+            <p>Thursday</p>
+            <input type="checkbox" id="5" value="0">
+          </li>
+          <li>
+            <p>Friday</p>
+            <input type="checkbox" id="6" value="0">
+          </li>
+          <li>
+            <p>Saturday</p>
+            <input type="checkbox" id="7" value="0">
+          </li>
+
+        </ul>
+      </div>
+      <button v-on:click="requestSignup">Request</button>
     </div>
 </template>
 
@@ -11,7 +42,7 @@
   export default {
     data(){
       return{
-        text: null
+        openDay: null
       }
     },
     methods:{
@@ -21,9 +52,51 @@
           navigateTo('/usrs')
         }
       },
+      setCheckBox(){
+        for (let index = 1; index <= 7; index++) {
+          const check = document.getElementById(index)
+          check.addEventListener('click', function(){
+            if (check.value == 0){
+              check.value = 1
+            }else{
+              check.value = 0
+            }
+          })
+        }
+      },
+      getCheckBox(){
+        let data = ""
+        for (let index = 1; index <= 7; index++) {
+          const check = document.getElementById(index).value
+          check.toString()
+          data += check
+        }
+        this.openDay = data
+      },
+      /*
+      .
+      .
+     image function
+      .
+      .
+      .
+      */
+      requestSignup(){
+        this.getCheckBox()
+        const buff = localStorage.getItem("h1")
+        let form = JSON.parse(buff)
+        form.push(this.openDay)
+        
+        const requestApi = async () => {
+          const data = await useSignUpForm(form[0],form[1],form[2],form[3],form[4],form[5],form[6],"","","",form[7],form[8],this.openDay)
+          localStorage.setItem("refer",data)
+        }
+        requestApi()
+      }
     },
     mounted: function() {
         this.$nextTick(this.islogin())
+        this.$nextTick(this.setCheckBox())
     }
   }
 </script>
