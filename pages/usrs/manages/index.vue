ed57afc02ecebf8cae7128367525e71de690b99e
Peerasit <peerasitearth@outlook.com>
2023-11-02 13:32:19 +0700

    Update UI manageProfile page

--- pages/usrs/manages/index.vue
+++ pages/usrs/manages/index.vue
@@ -1,21 +1,88 @@
 <template>
     <Header :logo="true" :logout="true" :home="true"/>
-    <h1>Manages</h1>
-    <img :src="logoImage" alt="Logo Restaurant">
+    
+    <div class="wrapper">
+        <div class="main">
+            <div class="header">
+                <div><p>Manage Profile</p></div>
+            </div>
+            <div class="back">
+                <img src="@/assets/ResPic.png" class="background" alt="" srcset="">
+            </div>
+            <div class="data">
+                <div class="image">
+                    <div class="circular">
+                        <img :src="logoImage" alt="Logo Restaurant Image">
+                    </div>
+                </div>
+
+                <div class="show">
+                    <div class="field">
+                        <div>
+                            <p class="name">Firstname</p>
+                            <div>
+                                <p>{{ firstName }}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="name">Lastname</p>
+                            <div>
+                                <p>{{ lastname }}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="name">Username</p>
+                            <div>
+                                <p>{{ username }}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="name">Email</p>
+                            <div>
+                                <p>{{ email }}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="name">Restaurant</p>
+                            <div>
+                                <p>{{ restaurant }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="pass">
+
+        </div>
+    </div>
+
 </template>
 
 <script setup lang="ts">
 import { fetchLogoImage } from "~/utils/userAPI";
+import { useUserStore } from "@/stores/userStore";
+
+const store = useUserStore()
 
 async function getLogo() {
     const path = await fetchLogoImage("earth1234")
     return "https://content-shibaqueue.doksakura.com" + path; 
 }
 
-let logoImage:string = await getLogo()
 
+let logoImage:string = await getLogo()
+//let name = store.username
+
+let firstName = "firstNameText"
+let lastname = "lastnameText"
+let username = "usernameText"
+let email = "emailText"
+let restaurant = "restaurantText"
 
 
 
 
 </script>
+<style scoped src="@/assets/styles/manageProfile.css"></style>
